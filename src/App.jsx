3c6a6fee810bee760fdcb35354d3d4e65337aa5e
sp1aca9fa <sp1aca9fa@gmail.com>
2024-05-27 23:58:37 +0900

    Starting project - replacing initial files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 function App() {
+  // Xx: the syntax of having html in js files is called jsx and thats why the files are .jsx
+  // Xx: this is not supported by the browser; it is transformed under the hood
   return <h1>Hello World!</h1>;
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+// Xx: importing a css file would normally not work in the browser, but it is a newer feature and is transformed under the hood.
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // Xx: this gets the element with root id from the HTML and renders the react code below
+  // Xx: strictMode is a feature in react which enables some additional checks and warns us of suboptimal code, including outdated code
+  // Xx: adding the App as an HTML element
+  // Xx: this is possible because of react components, which generally return JSX code (html inside js code; could return other code other than jsx)
+  // Xx: indeed, the App file simply returns the html code for hello world
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
