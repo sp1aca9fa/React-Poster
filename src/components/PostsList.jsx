bac38aa2a2f6f9dd05328858def46fa52e9bbd89
sp1aca9fa <sp1aca9fa@gmail.com>
2024-06-01 11:02:20 +0900

    Day 5 - Removing dummy posts, creating and loading posts from backend

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,5 +1,5 @@
 // Xx: while not required, it is considered good practice to put different components into different files
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Post from "./Post";
 import NewPost from "./NewPost";
@@ -9,10 +9,33 @@ import classes from "./PostsList.module.css";
 // Xx: useState: comments/explanation in NewPost.jsx, as it was originally implemented there
 
 function PostsList({ isPosting, onStopPosting }) {
+  // Xx: cannot use await async directly in functions in React components, because components should not return promises, only jsx, at least as a standard (we could use some react library? to handle it, apparently)
   const [posts, setPosts] = useState([]);
   // Xx: useState should be set to an array when the new state depends on the old state
 
+  // Xx: useEffect is the way to receive data from the backend by updating state and without running into an infinite loop (as we would if we just implemented the fetch without useEffect)
+  // Xx: useEffect takes in a function and an array as values/arguments; the array is to set the dependencies so React can check them and run the function again; if we leave as null, React will not run useEffect again
+  // Xx: did not set the unnamed function as async because useEffect should not take a function that return a promise itself; it should return nothing or a cleanup function
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+
+    fetchPosts();
+  }, []);
+
   function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }); // Xx: js function to handle HTTP requests, can be used to fetch and also send http requests
+    // Xx: this is what sends the request to the backend api
+
     // Xx: adds the new post as the first post and shows the previous posts
     // setPosts([postData, ...posts]);
     // Xx: general rule using states, if shouldng use existing posts as the above, we cant do just "...posts", we should actually pass a function to setPosts
@@ -29,9 +52,23 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        <Post author="Manuel" body="Check out the full course!" />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post
+              key={crypto.randomUUID()}
+              author={post.author}
+              body={post.body}
+            />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }
@@ -44,5 +81,13 @@ function PostsList({ isPosting, onStopPosting }) {
 
 // Xx: modalIsVisible && -> the way js works, the code after && will only run if modalIsVisible is true, so this is a quick implementation, instead of other implementations
 // Xx: the previous implementation was inputting the jsx code in a variable, which by the way, can be done
+
+// Xx: about the posts output: if simply input {posts}, the output would be an array of pure js objects, not of JSX elements. so we need to use map
+// Xx: map method takes a function that is executed for each item in the array, receiving that item and then returning the value to each it should be mapped, so we transform the item received into a JSX Post
+
+// Xx: about key: each child in a list should have a unique key, as required by react. usually we should make it so that the key is unique, but in this case, as a quick fix, we just use post.body as the key;
+// Xx: if 2 posts are made with the same body, the posts will show up initially without issues, but react may have issues later on rendering them (like omitting one)
+// Xx: key is a special prop, it does not need to be passed or anything like that
+// Xx: I replaced Max' solution of key={post.body} to key={Math.random()} initially; later I googled a simple unique id generator and updated it
 
 export default PostsList;
